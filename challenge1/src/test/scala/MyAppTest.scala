63b71104fabd5ea44451f4fc4c03e2a0fd6a331d
Michael Edwards <medwards@bitpusher.com>
2022-04-29 08:32:10 -0700

    A query that produces correct output, open-coded in the test

--- challenge1/src/test/scala/MyAppTest.scala
+++ challenge1/src/test/scala/MyAppTest.scala
@@ -1,6 +1,6 @@
 import org.scalatest.flatspec.AnyFlatSpec
 
-import org.apache.spark.sql.functions.col
+import org.apache.spark.sql.functions._
 
 class MyAppTest extends AnyFlatSpec with MASharedSparkContext  {
 
@@ -49,26 +49,34 @@ class MyAppTest extends AnyFlatSpec with MASharedSparkContext  {
       .option("header", false)
       .schema(schema = ratingsSchema)
       .csv(s"${basePath}/ratings/*/*")
+      .filter(col("user_id").isNotNull && col("movie_id").isNotNull && col("rating").isNotNull && col("ts").isNotNull)
 
     val df_ratings = df_ratings_raw
       .repartition(200, col("movie_id"))
       .sort(col("movie_id"), col("user_id"), col("ts").desc)
       .dropDuplicates("movie_id", "user_id")
 
-    val df_ratings_joined = df_ratings.join(df_movie_lookup, df_ratings("movie_id") === df_movie_lookup("movie_id"), "leftouter")
-                                      .select(df_ratings("movie_id"),
-                                              df_movie_lookup("movie_title"),
-                                              df_movie_lookup("movie_runtime"),
-                                              df_ratings("user_id"),
-                                              df_ratings("ts"),
-                                              df_ratings("rating"))
+    val df_ratings_avg = df_ratings
+      .groupBy("movie_id")
+      .agg(avg("rating").as("avg_movie_rating"), count("*").as("number_of_votes"))
 
-    df_ratings_joined.show(5, truncate = false)
+    val df_ratings_joined = df_ratings_avg.join(df_movie_lookup, df_ratings_avg("movie_id") === df_movie_lookup("movie_id"), "leftouter")
+                                          .select(df_ratings_avg("movie_id"),
+                                                  df_movie_lookup("movie_title"),
+                                                  df_movie_lookup("movie_runtime"),
+                                                  df_ratings_avg("avg_movie_rating"),
+                                                  df_ratings_avg("number_of_votes"))
+
     df_ratings_raw.groupBy(col("movie_id"), col("user_id")).count.orderBy(col("count").desc).show(5, truncate = false)
-    df_ratings_joined.groupBy(col("movie_id"), col("user_id")).count.orderBy(col("count").desc).show(5, truncate = false)
+    df_ratings.groupBy(col("movie_id"), col("user_id")).count.orderBy(col("count").desc).show(5, truncate = false)
+    df_ratings_joined.orderBy(col("number_of_votes").desc, col("avg_movie_rating").desc, col("movie_id")).show(truncate = false)
+
     printf("Raw row count = %d\n", df_ratings_raw.count)
-    printf("Deduplicated/joined row count = %d\n", df_ratings.count)
+    printf("Deduplicated row count = %d\n", df_ratings.count)
+    printf("Aggregated/joined row count = %d\n", df_ratings_joined.count)
+    df_ratings_raw.agg(countDistinct("movie_id")).show(truncate = false)
+
     df_ratings_raw.filter(col("user_id") === 207971).orderBy(col("movie_id"), col("ts")).show(truncate = false)
-    df_ratings_joined.filter(col("user_id") === 207971).orderBy(col("movie_id"), col("ts")).show(truncate = false)
+    df_ratings.filter(col("user_id") === 207971).orderBy(col("movie_id"), col("ts")).show(truncate = false)
   }
 }
